e15bb68cabb31c91ceb6573e468346ca0bf4634f
Philippe Ganz <philippe.ganz@gmail.com>
2021-03-20 18:28:24 +0100

    Change app message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send("This is a test and I'm awesome"));
+app.get('/', (req, res) => res.send("Yet another test !"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
